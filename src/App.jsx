2afd357c6e5f9e0fa25e31b593ad6b56a6ea04aa
Cristian26121997 <cristian.espina.r1997@gmail.com>
2023-11-21 15:54:39 -0300

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Cart from './components/Cart'
+import ProductDetail from './components/ProductDetail'
 const App = () => {
 
   
@@ -23,6 +24,9 @@ const App = () => {
         <Route exact path="/About" element={<About/>}/>
         <Route exact path="/Cart" element={<Cart/>}/>
         <Route exact path="/itemListContainer" element={<ItemListContainer/>}/>
+        <Route exact path="/producto/:id" element={<ProductDetail/>}/>
+        <Route exact path="/producto/:categoria" element={<ProductDetail/>}/>
+
 
 
 
